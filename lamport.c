70c5804fa72204a49fffcc8e31023afe5fc95b05
Toprak44Kmpl <82000599+Toprak44Kmpl@users.noreply.github.com>
2022-05-08 17:48:24 -0500

    1

--- lamport.c
+++ lamport.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
+DEFINE llave ?0
+
 
 int main(){
     char direcciones[][5] = {"192.168.1.70","192.168.1.88","192.168.1.127","192.168.1.91","192.168.1.100"}
     return 0;
+}
+
+
+int main(){
+	int numero=-1;
+
+	while(1){
+		if (numero == 155){
+			break;
+		}
+		srand(time(NULL)); 
+		numero=random()%500;
+		printf("%d\n",numero);	
+	}
+
+	return 0;
 }
