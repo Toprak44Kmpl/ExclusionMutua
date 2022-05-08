e1be2b598580ebdc7ae510d807b8e38a8becd4c0
Toprak44Kmpl <82000599+Toprak44Kmpl@users.noreply.github.com>
2022-05-08 17:51:23 -0500

    1

--- lamport.c
+++ lamport.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 
-DEFINE llave ?0
-
+int llave = 0
 
 int main(){
     char direcciones[][5] = {"192.168.1.70","192.168.1.88","192.168.1.127","192.168.1.91","192.168.1.100"}
-    return 0;
+    
+
+    if(llave == 1){
+        //solicitar tiempo local
+        //Enviar mensaje a todas máquinas el par (tiempo loca, dirección ip) 
+    }
+
+    
 }
 
 
 int main(){
 	int numero=-1;
-
 	while(1){
 		if (numero == 155){
-			break;
+			llave = 1
+            break;
 		}
 		srand(time(NULL)); 
 		numero=random()%500;
 		printf("%d\n",numero);	
 	}
-
 	return 0;
 }
